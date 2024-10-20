0b007ecbf03ba722f3009af0e1c33dfa51504dd3
Ariel Ortenzi <arielortenzi@gmail.com>
2024-10-20 09:30:38 -0300

    Varuable de entorno Key de BD

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAMxUL8ofGITzuuDB7uGf1oop06I8eaajQ",
+    apiKey: import.meta.env.VITE_BDKEY,
     authDomain: "orpack-e-commerce.firebaseapp.com",
     projectId: "orpack-e-commerce",
     storageBucket: "orpack-e-commerce.appspot.com",
